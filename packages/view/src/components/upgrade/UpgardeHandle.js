14dcfb568e7a9063ceed532c4bb7af87a8fd8548
vyshakg <vyshak.g.gowda@gmail.com>
2019-02-20 01:43:18 +0530

    added stripe component and fixed some bugs

--- packages/view/src/components/upgrade/UpgardeHandle.js
+++ packages/view/src/components/upgrade/UpgardeHandle.js
@@ -1,15 +1,18 @@
 import Axios from "axios";
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import ErrorNotification from "../notification/ErrorNotification";
 import SuccessNotification from "../notification/SuccessNotification";
 
 async function upgradeServer(tokenId) {
   try {
-    const response = await Axios.post("/api/upgrade", tokenId);
+    const response = await Axios.post("/api/upgrade", { tokenId: tokenId });
     if (response) {
       SuccessNotification({ message: "Successfuly upgraded your account" });
     }
-  } catch (err) {}
+  } catch (err) {
+    ErrorNotification({ message: "Payment didn't go through" });
+  }
 }
 
 function UpgardeHandle() {
