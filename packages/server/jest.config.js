4d544a54c2bde3d15f6bd5ea67d7068cb07eb688
vyshakg <vyshak.g.gowda@gmail.com>
2019-02-26 00:06:45 +0530

    lerna  gulp and uglify package install

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   globalSetup: "<rootDir>/src/test/index.js",
   preset: "ts-jest",
-  testEnvironment: "node"
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"]
 };
