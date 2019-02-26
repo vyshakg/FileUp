48dbe7c5febcf1c7a9a5ea95fd0267fca6c93639
vyshakg <vyshak.g.gowda@gmail.com>
2019-02-26 23:25:33 +0530

    removed dist from git

--- dist/test/setup.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const testDatabaseConnection_1 = require("./testDatabaseConnection");
-testDatabaseConnection_1.testDatabaseConnection(true).then(() => process.exit());
-//# sourceMappingURL=setup.js.map
