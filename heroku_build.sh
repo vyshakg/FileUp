8d19c39e32615c2cec4bb8205ad04d1d95c02024
vyshakg <vyshak.g.gowda@gmail.com>
2019-02-26 20:32:25 +0530

    heroku deploy initail check

---
+++ heroku_build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+yarn run build:app
+
+cp -r ./packages/server/build ./build
+cp  ./packages/server/.env ./build
+cp -r ./packages/view/build ./build/view/
+cp ./ormconfig.json ./build/
